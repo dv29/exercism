0b1947b054d7dd6fe4e6ba6d0fc174428bd8267e
Vora, Deep <deepvora92@gmail.com>
2020-03-26 02:02:38 -0500

    go bob;

---
+++ go/bob/bob.go
@@ -0,0 +1,37 @@
+// This is a "stub" file.  It's a little start on your solution.
+// It's not a complete solution though; you have to write some code.
+
+// Package bob should have a package comment that summarizes what it's about.
+// https://golang.org/doc/effective_go.html#commentary
+package bob
+
+import (
+  "strings"
+  "regexp"
+)
+
+var HasLetter = regexp.MustCompile(`[a-zA-Z]+`).MatchString
+
+// Hey should have a comment documenting it.
+func Hey(remark string) string {
+	// Write some code here to pass the test suite.
+	// Then remove all the stock comments.
+	// They're here to help you get started but they only clutter a finished solution.
+	// If you leave them in, reviewers may protest!
+  remark = string(regexp.MustCompile(`[\n \r\t]+`).ReplaceAll([]byte(remark), []byte("")))
+
+  if remark == "" {
+    return "Fine. Be that way!"
+  }
+
+  if strings.ToUpper(remark) == remark && HasLetter(remark) {
+    if remark[len(remark)-1] == '?' {
+      return "Calm down, I know what I'm doing!"
+    }
+    return "Whoa, chill out!"
+  }
+  if remark[len(remark)-1] == '?' {
+    return "Sure."
+  }
+	return "Whatever."
+}
