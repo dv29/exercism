1ab59c33758cb9a23b2cb0704e5f6518bc1bb59d
Vora, Deep <deepvora92@gmail.com>
2020-04-01 18:59:52 -0500

    go pascals-triangle update;

--- go/pascals-triangle/pascals_triangle.go
+++ go/pascals-triangle/pascals_triangle.go
@@ -1,34 +1,20 @@
 package pascal
 
-import (
-	"math"
-)
-
 func Triangle(n int) [][]int {
 	if n < 1 {
 		return [][]int{}
 	}
 
 	t := make([][]int, n)
+	t[0] = []int{1}
 
-	for i := 1; i <= n; i++ {
-		x := i - 1
-		t[x] = make([]int, i)
-		mid := int(math.Round(float64(i) / 2.0))
+	for i := 1; i < n; i++ {
+		t[i] = make([]int, i+1)
+		t[i][0], t[i][i] = 1, 1
 
-		t[x][0] = 1
-
-		for j := 2; j <= mid; j++ {
-			y := j - 1
-			if x > 1 && y > 0 {
-				t[x][y] = t[x-1][y-1] + t[x-1][y]
-			} else {
-				t[x][y] = j
-			}
-		}
-
-		for j := mid; j < i; j++ {
-			t[i-1][j] = t[i-1][i-j-1]
+		for j := 1; j < i/2+1; j++ {
+			t[i][j] = t[i-1][j-1] + t[i-1][j]
+			t[i][i-j] = t[i][j]
 		}
 	}
 
