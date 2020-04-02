d4c02643226d2e1e2e16eb15e9ddb2ffa7925708
Vora, Deep <deepvora92@gmail.com>
2020-04-02 11:19:52 -0500

    go sum-of-multiples update;

--- go/sum-of-multiples/sum_of_multiples.go
+++ go/sum-of-multiples/sum_of_multiples.go
@@ -4,7 +4,7 @@ func SumMultiples(limit int, divisors ...int) int {
 	sum := 0
 	for i := 1; i < limit; i++ {
 		for _, d := range divisors {
-			if d > 0 && i % d == 0 {
+			if d > 0 && i%d == 0 {
 				sum += i
 				break
 			}
